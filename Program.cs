522a88408e87177e0bc080753adcc9cb9fbc284b
Chris Gaus <chrisgaus@gmail.com>
2020-02-17 15:34:41 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace SocketServerStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipaddr = IPAddress.Any;
+
+            IPEndPoint ipep = new IPEndPoint(ipaddr, 23000);
+
+            listenerSocket.Bind(ipep);
+
+            listenerSocket.Listen(5);
+
+            Console.WriteLine("About to accept incoming connection.");
+
+            Socket client = listenerSocket.Accept();
+
+            Console.WriteLine("Client connected. " + client.ToString() + " - IP End Point: " + client.RemoteEndPoint.ToString());
+
+            byte[] buff = new byte[128];
+
+            int numberOfReceivedBytes = 0;
+
+            numberOfReceivedBytes = client.Receive(buff);
+
+            Console.WriteLine("Number of received bytes: " + numberOfReceivedBytes);
+
+            Console.WriteLine("Data sent by client is: " + buff);
+
+            string receivedText = Encoding.ASCII.GetString(buff, 0, numberOfReceivedBytes);
+
+            Console.WriteLine("Data sent by the client is: " + receivedText);
+            
+        }
+    }
+}
